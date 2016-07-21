23631d8be8bb586dbbf8ca3da5a091a8659432f3
Talwinder Singh <nijjar.login@gmail.com>
2016-07-21 11:19:46 -0400

    added inherited scope to solve the problem

--- app/src/ts/directives/addressDirective.ts
+++ app/src/ts/directives/addressDirective.ts
@@ -1,11 +1,17 @@
 export class AddressDirective implements ng.IDirective {
     restrict = 'E';
     templateUrl = './views/address.html';
+    scope= true;
+
     controller = ($scope: any) => { 
     	$scope.collapsed = false;
         
         $scope.collapseAddress = () => {
         	$scope.collapsed = true;
+        }
+
+        $scope.expandAddress = () => {
+        	$scope.collapsed = false;
         }
     };
 
